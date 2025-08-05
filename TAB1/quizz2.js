43eacf55ee031a073d249e3038e331e03338a40f
Mickyone33 <mickywan@live.fr>
2025-08-05 23:12:38 +0200

    update id questions

--- TAB1/quizz2.js
+++ TAB1/quizz2.js
@@ -1,7 +1,7 @@
 var quizJS = [
     {
         id: 1,
-        question: "Question 1.1",
+        question: "Question 2.1",
         options: [{ a: "a) mauvaise reponse", b: "b) bonne reponse", c: "c) mauvaise reponse", d: "d) mauvaise reponse" }],
         answer: "b",
         score: 0,
@@ -9,7 +9,7 @@ var quizJS = [
     },
     {
         id: 2,
-        question: "question 1.2",
+        question: "question 2.2",
         options: [{ a: "a) mauvaise reponse", b: "b) bonne reponse", c: "c) mauvaise reponse", d: "d) mauvaise reponse" }],
         answer: "b",
         score: 0,
@@ -17,7 +17,7 @@ var quizJS = [
     },
     {
         id: 3,
-        question: "question 1.3",
+        question: "question 2.3",
         options: [{ a: "a) mauvaise reponse", b: "b) bonne reponse", c: "c) mauvaise reponse", d: "d) mauvaise reponse" }],
         answer: "b",
         score: 0,
--- TAB1/quizz3.js
+++ TAB1/quizz3.js
@@ -1,7 +1,7 @@
 var quizJS = [
     {
         id: 1,
-        question: "Question 1.1",
+        question: "Question 3.1",
         options: [{ a: "a) mauvaise reponse", b: "b) mauvaise reponse", c: "c) bonne reponse", d: "d) mauvaise reponse" }],
         answer: "c",
         score: 0,
@@ -9,7 +9,7 @@ var quizJS = [
     },
     {
         id: 2,
-        question: "question 1.2",
+        question: "question 3.2",
         options: [{ a: "a) mauvaise reponse", b: "b) mauvaise reponse", c: "c) bonne reponse", d: "d) mauvaise reponse" }],
         answer: "c",
         score: 0,
@@ -17,7 +17,7 @@ var quizJS = [
     },
     {
         id: 3,
-        question: "question 1.3",
+        question: "question 3.3",
         options: [{ a: "a) mauvaise reponse", b: "b) mauvaise reponse", c: "c) bonne reponse", d: "d) mauvaise reponse" }],
         answer: "c",
         score: 0,
